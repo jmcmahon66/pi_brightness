aebfe76206eebd6461bb7b5c99c3423cc8a49e7f
jmcmahon <j.a.mcmahon66@gmail.com>
2023-10-24 03:41:38 +0100

    Fixed setup.py Internal dependencies don't need to be declared

--- setup.py
+++ setup.py
@@ -2,13 +2,8 @@ from setuptools import setup
 
 setup(
     name='pi_brightness',
-    version='1.0.0',
+    version='1.0.1',
     description='Controls Raspberry Pi screen brightness (or other Linux devices)',
     author='Joseph McMahon',
     packages=['module'],
-    install_requires=[
-        'os',
-        'enum',
-        'subprocess',
-    ],
 )
