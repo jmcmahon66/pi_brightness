f3b840cf3e7fedbd6b7b13200aef5928eec11743
jmcmahon <j.a.mcmahon66@gmail.com>
2023-10-24 01:01:19 +0100

    Updates brightness file now Updated unit tests

--- module/pi_brightness.py
+++ module/pi_brightness.py
@@ -1,5 +1,6 @@
 import os
-import sys
+from enum import Enum
+import subprocess
 
 # Uses Linux Kernel GPU driver registers, should be supported by most Linux platforms
 # https://docs.kernel.org/gpu/backlight.html
@@ -9,6 +10,15 @@ default_backlight_dir = "/sys/class/backlight/"
 brightness_file = "brightness"
 
 
+# Error codes
+class ErrorCode(Enum):
+    PATH_NOT_FOUND = 1
+    INT_OUT_OF_RANGE = 2
+    UPDATE_FAILED = 3
+    TOO_MANY_DEVICES = 4
+    FILE_DOESNT_EXIST = 5
+
+
 def find_backlight_path(custom_backlight_dir=None):
     # Check if /sys/class/backlight/ exists
     # backlight_dir = "/sys/class/backlight/"
@@ -16,7 +26,7 @@ def find_backlight_path(custom_backlight_dir=None):
     # print(f"backlight_dir = {backlight_dir}")
 
     if not os.path.exists(backlight_dir) or not os.path.isdir(backlight_dir):
-        return None
+        return ErrorCode.PATH_NOT_FOUND
 
     # Get list of subdirectories in /sys/class/backlight/
     backlight_subdirs = [d for d in os.listdir(backlight_dir) if os.path.isdir(os.path.join(backlight_dir, d))]
@@ -29,9 +39,64 @@ def find_backlight_path(custom_backlight_dir=None):
         if os.path.isfile(brightness_file_path):
             return brightness_file_path
         else:
-            return None
+            return ErrorCode.FILE_DOESNT_EXIST
     else:
-        return None
+        return ErrorCode.TOO_MANY_DEVICES
+
+
+def translate_percentage_brightness(brightness):
+    brightness = int((int(brightness) / 100) * 255)
+    return brightness
+
+
+def update_brightness(brightness, brightness_file_path=None):
+    # Validate user input
+    # print(f"brightness before: {brightness}")
+    if 0 <= int(brightness) <= 100:
+        brightness = translate_percentage_brightness(brightness)
+        # print(f"brightness after: {brightness}")
+    else:
+        return ErrorCode.INT_OUT_OF_RANGE
+
+    backlight_path = find_backlight_path(brightness_file_path)
+    # if not isinstance(backlight_path, ErrorCode) or backlight_path not in ErrorCode:
+    #     # print(f"brightness_file_path: {brightness_file_path}")
+    #     print(f"backlight_path: {backlight_path}")
+    #     return 0
+    # else:
+    #     return backlight_path
+
+    if isinstance(backlight_path, ErrorCode):
+        return backlight_path
+
+    # TODO: Better way of doing this?
+    command = f"echo {brightness} | sudo tee {backlight_path}"
+    exit_code = subprocess.call(command, shell=True)
+
+    # command = f"cat {backlight_path}"
+    # output = subprocess.call(command, shell=True)
+
+    with open(backlight_path, 'r') as file:
+        # Read the contents of the file
+        output = file.read()
+
+    if exit_code == 0:
+        print(f"backlight value from file: {output}")
+        return 0
+    else:
+        return ErrorCode.UPDATE_FAILED
+
+    # brightness = int((int(brightness) / 100) * 255)
+    # return brightness
+
+    # Command to write to the brightness file
+    # brightness_value = 100  # Replace with your desired brightness value
+    # command = f"echo {brightness_value} | sudo tee /sys/class/backlight/10-0045/brightness"
+
+    # Execute the command with sudo
+    # subprocess.call(command, shell=True)
+
+    # echo "100" | sudo tee /sys/class/backlight/10-0045/brightness
 
 
 dir = find_backlight_path()
--- tests/backlight_tests.py
+++ tests/backlight_tests.py
@@ -1,6 +1,9 @@
 import unittest
 import sys
 import os
+import time
+
+# To be run on raspberry PI/Linux
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
 # Set the working directory to the script's directory
@@ -14,26 +17,98 @@ windows_path = "C:\\Workspace\\pi_brightness\\test"  # Need to make dir
 
 class TestBacklightPath(unittest.TestCase):
     def test_default_path(self):
+        print("Running test:", self._testMethodName)
         # Test with a valid path
-        path = find_backlight_path()
-        self.assertIsNotNone(path)
+        test_return = find_backlight_path()
+        self.assertIsNotNone(test_return, msg=f"path = {test_return}")
+        print(self._testMethodName, " - PASSED")
+        print("-----")
 
     def test_valid_path(self):
+        print("Running test:", self._testMethodName)
         # Test with a valid path
-        path = find_backlight_path(default_backlight_dir)
-        self.assertIsNotNone(path)
+        test_return = find_backlight_path(default_backlight_dir)
+        self.assertIsNotNone(test_return, msg=f"path = {test_return}")
+        print(self._testMethodName, " - PASSED")
+        print("-----")
 
     def test_invalid_path(self):
-        path = find_backlight_path("/invalid/")
-        self.assertIsNotNone(path)
+        print("Running test:", self._testMethodName)
+        test_return = find_backlight_path("/invalid/")
+        self.assertEqual(test_return, ErrorCode.PATH_NOT_FOUND)
+        print(self._testMethodName, " - PASSED")
+        print("-----")
 
+    # REMOVE
     def test_windows_path(self):
-        path = find_backlight_path(windows_path)
-        self.assertIsNotNone(path)
+        print("Running test:", self._testMethodName)
+        test_return = find_backlight_path(windows_path)
+        self.assertIsNotNone(test_return, msg=f"for testing on Windows - can ignore")
+        print(self._testMethodName, " - PASSED")
+        print("-----")
 
     # def test_multiple_subdirs(self):
 
     # def test_no_subdir(self):
+
+    def test_brightness_high(self):
+        print("Running test:", self._testMethodName)
+        test_return = update_brightness("100")
+        self.assertEqual(test_return, 0)
+        print(self._testMethodName, " - PASSED")
+        print("-----")
+        time.sleep(3)  # wait for screen tests on pi
+
+    def test_brightness_low(self):
+        print("Running test:", self._testMethodName)
+        test_return = update_brightness("0")
+        self.assertEqual(test_return, 0)
+        print(self._testMethodName, " - PASSED")
+        print("-----")
+        time.sleep(3)  # wait for screen tests on pi
+
+    def test_brightness_medium(self):
+        print("Running test:", self._testMethodName)
+        test_return = update_brightness("30")
+        self.assertEqual(test_return, 0)
+        print(self._testMethodName, " - PASSED")
+        print("-----")
+        time.sleep(3)  # wait for screen tests on pi
+
+    # Test that number in range 0-100 is a translated to whole number in range 0-255
+    def test_number_is_int_in_range(self):
+        print("Running test:", self._testMethodName)
+        lower_bound = 0
+        upper_bound = 255
+        loop_limit = 100
+
+        # Iterate through values from lower_bound to upper_bound (inclusive)
+        for value in range(lower_bound, loop_limit + 1):
+            brightness = translate_percentage_brightness(value)
+            print(f"Test BRIGHTNESS = {brightness}")
+            # Assert that the value is a whole number
+            self.assertAlmostEqual(brightness, round(brightness), delta=0, msg=f"Value {brightness} is not a whole number.")
+
+            # Assert that the value is within the specified range
+            self.assertGreaterEqual(brightness, lower_bound, msg=f"Value {brightness} is below the lower bound {lower_bound}.")
+            self.assertLessEqual(brightness, upper_bound, msg=f"Value {brightness} is above the upper bound {upper_bound}.")
+
+        print(self._testMethodName, " - PASSED")
+        print("-----")
+
+    def test_brightness_path(self):
+        print("Running test:", self._testMethodName)
+        test_return = update_brightness("80", default_backlight_dir)
+        self.assertEqual(test_return, 0)
+        print(self._testMethodName, " - PASSED")
+        print("-----")
+
+    def test_brightness_above_range(self):
+        print("Running test:", self._testMethodName)
+        return_code = update_brightness("101")
+        self.assertEqual(return_code, ErrorCode.INT_OUT_OF_RANGE)
+        print(self._testMethodName, " - PASSED")
+        print("-----")
 
 
 if __name__ == "__main__":
